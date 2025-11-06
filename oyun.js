b289307b85352a0e2d8fd60f93b7950051b6fbe3
nidaalisik <43543831+nidaalisik@users.noreply.github.com>
2025-11-06 13:27:28 +0300

    Update oyun.js

--- oyun.js
+++ oyun.js
@@ -70,7 +70,7 @@ async function baslatOyun() {
   // DALGA AYARLARI → İNCE + EKRANA OTURUYOR!
   dalga = {
     x: 0,
-    baseY: canvas.height * 0.25,  // DALGA DENİZ YÜZEYİNDE
+    baseY: canvas.height * 0.18,  // DALGA DENİZ YÜZEYİNDE
     width: canvas.width,
     height: canvas.height * 0.80,  // İNCE YÜKSEKLİK
     zaman: 0
@@ -131,5 +131,6 @@ async function baslatOyun() {
   }
   dongu();
 }
+
 
 
