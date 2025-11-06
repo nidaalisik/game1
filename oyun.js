9f6a52a267b1a2581a5189a75707784169798a10
nidaalisik <43543831+nidaalisik@users.noreply.github.com>
2025-11-06 22:25:39 +0300

    Update oyun.js

--- oyun.js
+++ oyun.js
@@ -69,7 +69,7 @@ async function baslatOyun() {
   // DALGA: DİKEY RESMİ YATAYDA UZAT + ALTTAN GÖSTER
   dalga = {
     x: 0,
-    gorunenYukseklik: canvas.height * 0.22,
+    gorunenYukseklik: canvas.height * 0.15,
     baslangicY: canvas.height * 0.25,// Ekranda görünen dalga yüksekliği
     zaman: 0
   };
@@ -137,5 +137,6 @@ async function baslatOyun() {
   }
   dongu();
 }
+
 
 
